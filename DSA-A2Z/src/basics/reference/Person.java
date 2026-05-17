bc3c1d489b13422e4158b734905a103917dbd022
yuvraj <46413950+y4yuvraj@users.noreply.github.com>
2026-05-17 23:55:53 +0530

    understanding java references

---
+++ DSA-A2Z/src/basics/reference/Person.java
@@ -0,0 +1,37 @@
+package basics.reference;
+
+public class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
+
+class Main{
+    public static void main(String [] args) {
+        Person p= new Person("yuv");
+        change(p);
+        System.out.println(p);
+    }
+
+    static void change(Person c)
+    {
+        c.setName("vuy");
+    }
+}
